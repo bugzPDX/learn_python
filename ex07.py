acdb0b58a4a06c45e41edefae954a80ce70e8040
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-01-24 11:57:54 -0800

    Added comments

--- ex07.py
+++ ex07.py
@@ -1,8 +1,13 @@
+# Prints string
 print "Mary had a little lamb."
+# Prints string and formatted variable
 print "Its fleece was white as %s." % 'snow'
+# Prints string
 print "And everywhere that Mary went."
-print "." * 10 # what'd that do?
+# Prints a period ten times
+print "." * 10  # what'd that do?
 
+# These lines set variables equal to strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +21,9 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+
 # watch that comma at the end. try removing it to see what happens
+# These lines print the previous variables.
+# The comma creates a space between them.
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
