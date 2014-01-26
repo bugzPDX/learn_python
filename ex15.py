b2756a00a7dca43f5ed4ebe01fe941bc455a0d40
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-01-25 18:13:40 -0800

    Added comments and made files python compliant

--- ex15.py
+++ ex15.py
@@ -1,18 +1,30 @@
+# Imports argv for use in this script
 from sys import argv
 
+# Sets script and filename variables to values obtained from argv
 script, filename = argv
 
+# Sets variable text equal to the command to open whatever
+# file was input on the command line.
 txt = open(filename)
 
+# Prints the string with the formatted variable
 print "Here's your file %r:" % filename
+# Prints the content of the file contained in variable txt
 print txt.read()
 
+# Prints the string
 print "Type the filename again:"
+# Sets variable equal to user input
 file_again = raw_input("> ")
 
+# Sets variable equal to contents of previous user input
 txt_again = open(file_again)
 
+# Prints the content of the data contained in txt_again variable
 print txt_again.read()
 
+# Closes txt file
 txt.close()
+# Closes txt_again file
 txt_again.close()
