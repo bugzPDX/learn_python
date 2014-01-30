bfd75010c9f9463254971c94d4ca21fc0b5e8f17
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-01-30 15:39:17 -0800

    Added some additional code to improve function

--- ex33.py
+++ ex33.py
@@ -1,13 +1,14 @@
 def my_function():
     i = 0
     numbers = []
-    max = 6
+    max = 20
+    increment = int(raw_input('Increment by: '))
 
-    while i < max:
+    while i <= max:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i += 1
+        i += increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
