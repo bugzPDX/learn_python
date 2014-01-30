4855881cd1d5b158df90eeaf600aa3c9ec03fc7a
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-01-29 22:20:28 -0800

    Fixed a formatting error

--- ex28.py
+++ ex28.py
@@ -22,8 +22,8 @@ print 'not (10 == 1 or 1000 == 1000) is %r' % (not (10 == 1 or 1000 == 1000))
 print 'not (1 != 10 or 3 == 4) is %r' % (not (1 != 10 or 3 == 4))
 print 'not ("testing" == "testing" and "Zed" == "Cool Guy") is %r' % (
     not ("testing" == "testing" and "Zed" == "Cool Guy"))
-print '1 == 1 and not ("testing" == 1 or 1 == 0) is %r' % 1 == 1 and not (
-    "testing" == 1 or 1 == 0)
+print '1 == 1 and not ("testing" == 1 or 1 == 0) is %r' % (1 == 1 and not (
+    "testing" == 1 or 1 == 0))
 print '"chunky" == "bacon" and not (3 == 4 or 3 == 3) is %r' % (
     "chunky" == "bacon" and not (3 == 4 or 3 == 3))
 print '3 == 3 and not ("testing" == "testing" or "Python" == "Fun") is %r' % (
