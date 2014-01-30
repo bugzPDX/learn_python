f995e759f6475054a85fb3c107bb813795279428
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-01-29 17:27:11 -0800

    Completed exercise 27. Modified class notes.

--- ex04.py
+++ ex04.py
@@ -1,7 +1,10 @@
+# Initialize variables
 cars = 100
 space_in_a_car = 4
 drivers = 30
 passengers = 90
+
+# Set calculations to new variables
 cars_not_driven = cars - drivers
 cars_driven = drivers
 carpool_capacity = cars_driven * space_in_a_car
