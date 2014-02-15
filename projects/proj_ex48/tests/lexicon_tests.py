778bc5e8251c11546dd25d936728a2e3446547a5
Lisa Hewus Fresh <lhewusfresh@gmail.com>
2014-02-14 16:37:27 -0800

    Added a test file for exercise 48. Has errors though.
    I am having an indentation error on this file currently.

--- projects/proj_ex48/tests/lexicon_tests.py
+++ projects/proj_ex48/tests/lexicon_tests.py
@@ -3,7 +3,7 @@ from ex48 import lexicon
 
 def test_directions():
 	assert_equal(lexicon.scan('north'), [('direction')])
-    result = lexicon.scan('north south east')
+    result = lexicon.scan("north south east")
     assert_equal(result, [('direction', 'north'),
                           ('direction', 'south'),
                           ('direction', 'east')])
